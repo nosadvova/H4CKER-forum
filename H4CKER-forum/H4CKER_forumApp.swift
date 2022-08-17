3207a59e3c249517dd0c85afa0a975d212f819f5
donVolodya <volodymyr.novosad.knm.2020@lpnu.ua>
2022-08-17 19:15:49 +0300

    Initial Commit

---
+++ H4CKER-forum/H4CKER_forumApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4CKER_forumApp.swift
+//  H4CKER-forum
+//
+//  Created by Vova Novosad on 17.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct H4CKER_forumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
